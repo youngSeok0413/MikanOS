4fa01265db9c3528eeb52ce4a12deb36879ed6f2
yschoi <youngseok0413@gmail.com>
2024-01-02 15:37:58 +0900

    osbook_day05a

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -3,6 +3,29 @@
 
 #include "frame_buffer_config.hpp"
 
+/*
+ *font example : A
+ */
+const uint8_t kFontA[16] =
+    {
+        0b00000000, //
+        0b00011000, //    **
+        0b00011000, //    **
+        0b00011000, //    **
+        0b00011000, //    **
+        0b00100100, //   *  *
+        0b00100100, //   *  *
+        0b00100100, //   *  *
+        0b00100100, //   *  *
+        0b01111110, //  ******
+        0b01000010, //  *    *
+        0b01000010, //  *    *
+        0b01000010, //  *    *
+        0b11100111, // ***  ***
+        0b00000000, //
+        0b00000000, //
+};
+
 struct PixelColor
 {
     uint8_t r, g, b;
@@ -55,6 +78,28 @@ public:
     }
 };
 
+/**
+ * write ascii
+ */
+void WriteAscii(PixelWriter &writer, int x, int y, char c, const PixelColor &color)
+{
+    if (c != 'A')
+        return;
+
+    for (int dy = 0; dy < 16; dy++)
+    {
+        for (int dx = 0; dx < 8; dx++)
+        {
+            if((kFontA[dy] << dx) & 0x80u)
+            {
+                writer.Write(x+dx, y+dy, color);
+            }
+        }
+    }
+
+    return;
+}
+
 // operator new, delete
 void *operator new(size_t size, void *buf)
 {
@@ -97,5 +142,10 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config)
         }
     }
 
-    while (1) __asm__("hlt");
+    //test : write ascii(A)
+    WriteAscii(*pixel_writer, 50, 50, 'A', {0, 0, 0});
+    WriteAscii(*pixel_writer, 58, 50, 'A', {0, 0, 0});
+
+    while (1)
+        __asm__("hlt");
 }
