5eccbc5f40e3005e112dd36a52d585651c861c7e
yschoi <youngseok0413@gmail.com>
2024-01-02 17:14:52 +0900

    osbook_day05f

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -20,8 +20,28 @@ void operator delete(void *obj) noexcept
 {
 }
 
+// pixel writer
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter *pixel_writer;
+
+// console
+char console_buf[sizeof(Console)];
+Console *console;
+
+// printk
+int printk(const char *format, ...)
+{
+    va_list ap;
+    int result;
+    char s[1024];
+
+    va_start(ap, format);
+    result = vsprintf(s, format, ap);
+    va_end(ap);
+
+    console->PutString(s);
+    return result;
+}
 
 extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config)
 {
@@ -45,13 +65,11 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config)
         }
     }
 
-    Console console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
+    console = new (console_buf) Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
 
-    char buf[128];
     for (int i = 0; i < 27; ++i)
     {
-        sprintf(buf, "line %d\n", i);
-        console.PutString(buf);
+        printk("printk: %d\n", i);
     }
 
     while (1)
--- kernel/newlib_support.c
+++ kernel/newlib_support.c
@@ -1,6 +1,8 @@
 #include <sys/types.h>
+#include <errno.h>
 
 caddr_t sbrk(int incr) 
 {
-  return NULL;
+  errno = ENOMEM;
+  return (caddr_t)-1;
 }
